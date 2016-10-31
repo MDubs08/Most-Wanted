b2359af6c1c4bd3d719ea2138040c1eb455002b0
Mike Wilson <mdubs08@gmail.com>
2016-10-31 11:26:28 -0500

    Initial drop
    Most Wanted Project

---
+++ App.js
@@ -0,0 +1,683 @@
+var dataObject = { 
+	"272822514" : { 
+	  "firstName": "Billy",
+	  "lastName": "Bob",
+	  "gender": "male",
+	  "dob": "1/18/1949",
+	  "height": 71,
+	  "weight": 175,
+	  "eyeColor": "brown",
+	  "occupation": "programmer",
+	  "parents": [],
+	  "currentSpouse": 401222887 
+	},
+	"401222887" : { 
+	  "firstName": "Uma",
+	  "lastName": "Bob",
+	  "gender": "female",
+	  "dob": "4/1/1947",
+	  "height": 65,
+	  "weight": 162,
+	  "eyeColor": "brown",
+	  "occupation": "assistant",
+	  "parents": [],
+	  "currentSpouse": 272822514 
+	},
+	"409574486" : { 
+	  "firstName": "Michael",
+	  "lastName": "Walkens",
+	  "gender": "male",
+	  "dob": "5/9/1951",
+	  "height": 76,
+	  "weight": 250,
+	  "eyeColor": "brown",
+	  "occupation": "landscaper",
+	  "parents": [],
+	  "currentSpouse": 260451248 
+	},
+	"260451248" : {
+	  "firstName": "Jon",
+	  "lastName": "Walkens",
+	  "gender": "male",
+	  "dob": "9/6/1945",
+	  "height": 62,
+	  "weight": 115,
+	  "eyeColor": "brown",
+	  "occupation": "assistant",
+	  "parents": [],
+	  "currentSpouse": 409574486 
+	},
+	"629807187" : {
+	  "firstName": "Jack",
+	  "lastName": "Pafoy",
+	  "gender": "male",
+	  "dob": "3/16/1938",
+	  "height": 70,
+	  "weight": 207,
+	  "eyeColor": "black",
+	  "occupation": "nurse",
+	  "parents": [],
+	  "currentSpouse": 464142841 
+	},
+	"464142841" : {
+	  "firstName": "Jen",
+	  "lastName": "Pafoy",
+	  "gender": "female",
+	  "dob": "4/10/1940",
+	  "height": 72,
+	  "weight": 256,
+	  "eyeColor": "black",
+	  "occupation": "student",
+	  "parents": [],
+	  "currentSpouse": 629807187 
+	},
+	"982411429" : {
+	  "firstName": "Mister",
+	  "lastName": "Potatoo",
+	  "gender": "male",
+	  "dob": "12/18/1952",
+	  "height": 66,
+	  "weight": 170,
+	  "eyeColor": "hazel",
+	  "occupation": "architect",
+	  "parents": [],
+	  "currentSpouse": 595767575 
+	},
+	"595767575" : {
+	  "firstName": "Missuz",
+	  "lastName": "Potatoo",
+	  "gender": "female",
+	  "dob": "10/28/1948",
+	  "height": 59,
+	  "weight": 137,
+	  "eyeColor": "blue",
+	  "occupation": "architect",
+	  "parents": [],
+	  "currentSpouse": 982411429 
+	},
+	"693243224" : {
+	  "firstName": "Joy",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "4/20/1939",
+	  "height": 69,
+	  "weight": 199,
+	  "eyeColor": "hazel",
+	  "occupation": "doctor",
+	  "parents": [],
+	  "currentSpouse": 888201200 
+	},
+	"888201200" : {
+	  "firstName": "Mader",
+	  "lastName": "Madden",
+	  "gender": "male",
+	  "dob": "5/6/1937",
+	  "height": 76,
+	  "weight": 205,
+	  "eyeColor": "black",
+	  "occupation": "landscaper",
+	  "parents": [],
+	  "currentSpouse": 693243224  
+	},
+	"878013758" : {
+	  "firstName": "Jill",
+	  "lastName": "Pafoy",
+	  "gender": "female",
+	  "dob": "2/8/1972",
+	  "height": 74,
+	  "weight": 118,
+	  "eyeColor": "brown",
+	  "occupation": "programmer",
+	  "parents": [401222887,272822514],
+	  "currentSpouse": 294874671 
+	},
+	"951747547" : {
+	  "firstName": "Ralph",
+	  "lastName": "Bob",
+	  "gender": "male",
+	  "dob": "12/23/1969",
+	  "height": 66,
+	  "weight": 179,
+	  "eyeColor": "blue",
+	  "occupation": "nurse",
+	  "parents": [401222887,272822514],
+	  "currentSpouse": 159819275 
+	},
+	"159819275" : {
+	  "firstName": "Jasmine",
+	  "lastName": "Bob",
+	  "gender": "female",
+	  "dob": "12/18/1969",
+	  "height": 58,
+	  "weight": 156,
+	  "eyeColor": "blue",
+	  "occupation": "assistant",
+	  "parents": [409574486,260451248],
+	  "currentSpouse": 951747547 
+	},
+	"348457184" : {
+	  "firstName": "Annie",
+	  "lastName": "Pafoy",
+	  "gender": "female",
+	  "dob": "11/4/1970",
+	  "height": 62,
+	  "weight": 235,
+	  "eyeColor": "hazel",
+	  "occupation": "landscaper",
+	  "parents": [629807187,464142841],
+	  "currentSpouse": null 
+	},
+	"294874671" : {
+	  "firstName": "Dave",
+	  "lastName": "Pafoy",
+	  "gender": "male",
+	  "dob": "8/5/1967",
+	  "height": 61,
+	  "weight": 112,
+	  "eyeColor": "green",
+	  "occupation": "doctor",
+	  "parents": [629807187,464142841],
+	  "currentSpouse": 878013758 
+	},
+	"931247228" : {
+	  "firstName": "Amii",
+	  "lastName": "Pafoy",
+	  "gender": "female",
+	  "dob": "3/13/1963",
+	  "height": 74,
+	  "weight": 184,
+	  "eyeColor": "brown",
+	  "occupation": "landscaper",
+	  "parents": [629807187,464142841],
+	  "currentSpouse": null 
+	},
+	"822843554" : {
+	  "firstName": "Regina",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "7/26/1959",
+	  "height": 71,
+	  "weight": 249,
+	  "eyeColor": "brown",
+	  "occupation": "nurse",
+	  "parents": [693243224, 888201200],
+	  "currentSpouse": null 
+	},
+	"819168108" : {
+	  "firstName": "Hana",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "10/7/1953",
+	  "height": 70,
+	  "weight": 187,
+	  "eyeColor": "brown",
+	  "occupation": "politician",
+	  "parents": [693243224, 888201200],
+	  "currentSpouse": null 
+	},
+	"969837479" : {
+	  "firstName": "Eloise",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "12/11/1961",
+	  "height": 63,
+	  "weight": 241,
+	  "eyeColor": "brown",
+	  "occupation": "assistant",
+	  "parents": [693243224, 888201200],
+	  "currentSpouse": null 
+	},
+	"313207561" : {
+	  "firstName": "Mattias",
+	  "lastName": "Madden",
+	  "gender": "male",
+	  "dob": "2/19/1966",
+	  "height": 70,
+	  "weight": 110,
+	  "eyeColor": "blue",
+	  "occupation": "assistant",
+	  "parents": [693243224, 888201200],
+	  "currentSpouse": 313997561 
+	},
+	"313997561" : {
+	  "firstName": "Ellen",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "2/19/1970",
+	  "height": 67,
+	  "weight": 100,
+	  "eyeColor": "blue",
+	  "occupation": "doctor",
+	  "parents": [],
+	  "currentSpouse": 313207561 
+	},
+	"313998000" : {
+	  "firstName": "Joey",
+	  "lastName": "Madden",
+	  "gender": "female",
+	  "dob": "2/02/1987",
+	  "height": 67,
+	  "weight": 100,
+	  "eyeColor": "blue",
+	  "occupation": "doctor",
+	  "parents": [313207561,313997561],
+	  "currentSpouse": null 
+	}
+};
+
+var people = [];
+peopleArray();
+var agent = agentLogin();
+searchBy();
+
+function searchBy(){
+	var searchOption = prompt("Welcome back agent " + agent + ". What information are you looking for? \n\nPerson \nTrait \nExit");
+	switch(searchOption.toLowerCase())
+	{
+		case "person":
+		personSearch();
+		break;
+		
+		case "trait":
+		//alert("Agent " + agent + " does not have authorization to this feature.");
+		//searchBy();
+		traitSearch();
+		break;
+		
+		case "exit":
+		alert("Goodbye agent!")
+		window.close();
+		break;
+		
+		default:
+		alert("That is not an acceptible option agent.")
+		searchBy();
+	}
+}
+
+function agentLogin(){
+	alert("FBI Statement: If you are not an authorized FBI special forces agent, close this window now!");
+	agent = prompt("Enter your U.S. Government provided user name to continue.")
+	return agent;
+}
+
+function personSearch(){
+	var fullName = prompt("Enter criminal's first and last name.");
+	var [firstName, lastName] = fullName.split(" ");
+	//confirm("Are you looking for "+ firstName + " " + lastName + "? Press OK to continue.");
+	var person = getPersonInfo(firstName, lastName);
+	searchByPerson(person);
+}
+
+function traitSearch(){
+	var traits = [];	
+	var traitSearch = prompt("Please type your search terms separated by commas \n\nYou can search up to five of the following options: \n\nAge - Only in the format # \nAge Range - Only in the format #-# \nHeight - only in the format #’#” \nWeight - only in the format #lbs \nOccupation - Single word \nEye Color - Single word \n\nExample: 30,blue,programmer,6'5'',150lbs \n\nType 'Exit' to exit or 'New Search' for a new search");
+	if (traitSearch.toLowerCase() != "exit" || traitSearch.toLowerCase() != "new search"){
+		traits.push(traitSearch.split(","));
+		alert(getTraitInfo(traits));
+	}
+	else if (traitSearch.toLowerCase == "new search"){
+		searchBy();
+	}
+	else if (traitSearch.toLowerCase == "exit"){
+		alert("Goodbye agent " + agent);
+		window.close();
+	}
+	traitSearch();
+}
+
+function searchByPerson(person){
+	do{
+		var choice = prompt ("What information for " + person.firstName + " " + person.lastName + " would you like to look for? \n\nPortfolio \nDescendants \nFamily \nNext Kin \nNew Search \nExit");
+		switch(choice.toLowerCase()){
+			case "portfolio":
+			alert("Portfolio: \n\n" + portfolio(person));
+			break;
+		
+			case "descendants":
+			alert("Descendants: \n\n" + descendants(person));
+			break;
+		
+			case "family":
+			alert("Immediate Family: \n\n" + family(person));
+			break;
+		
+			case "next kin":
+			alert("Next of kin: \n\n" + kin(person));
+			break;
+		
+			case "new search":
+			searchBy();
+			break;
+		
+			case "exit":
+			alert("Goodbye agent " + agent + "!");
+			window.close();
+			break;
+		
+			default:
+			alert(choice + " is an invalid selection.")
+			searchByPerson(person);
+		}
+	}while (choice != "exit")
+}
+
+function getFullName(id){
+	var getFullName = [];
+	for (var i=0; i < people.length; i++){
+			for (var j=0; j < id.length; j++){
+				if (people[i].id == id[j]){
+					var fullName = (people[i].firstName + " " + people[i].lastName);
+					getFullName.push(fullName);
+			}
+		}
+	}
+	return (getFullName.join("\n"));
+}
+
+function getPersonInfo(firstname, lastname){
+	for (var i=0; i < people.length; i++){
+		if (people[i].firstName.toLowerCase() == firstname.toLowerCase() && people[i].lastName.toLowerCase() == lastname.toLowerCase()){
+		return people[i];	
+		}
+	}
+	alert("Unable to find criminal");
+	personSearch();	
+}
+
+function getPersonInfoById(id){
+	var personById = [];
+	for (var i=0; i < people.length; i++){
+		if (people[i].id == id){
+			var result = people[i];
+			personById.push(result);
+		}
+		else{
+			continue;
+		}
+	}
+	return personById;
+}
+
+function portfolio(person){
+	var attribute = [];
+	var results = ("Full Name: " + person.firstName + " " + person.lastName + "\nGender: " + person.gender + "\nDate of Birth: " + person.dob + "\nAge: " + getAge(person.dob) + "\nHeight: " + getHeight(person.height) + "\nWeight: " + getWeight(person.weight) + "\nEye Color: " + person.eyeColor + "\nOccupation: " + person.Occupation + "\nSpouse: " + getFullName(spouse(person)));
+	attribute.push(results);
+	return(attribute.join("\n"));
+}
+
+function descendants(person){
+	var firstGen = (children(person));
+	var secondGen = [];
+	for(var i=0; i < firstGen.length;i++){
+		for(var j=0; j < people.length;j++){
+			if(people[j].parents.length !=0){
+				if(people[j].parents[0] == firstGen[i] || people[j].parents[1] == firstGen[i]){
+					var result = (people[i].id);
+					secondGen.push(result);
+				}
+			}
+		}
+	}
+	var descendants = [];
+	descendants = ("First Generation: \n" + getFullName(firstGen).concat("\n\nSecond Generation: \n" + getFullName(secondGen)));
+	return (descendants);
+}
+
+function children(person){
+	var children = [];
+	for (var i = 0; i < people.length; i++) {
+		if(people[i].parents.length != 0){
+			if(people[i].parents[0] == person.id || people[i].parents[1] == person.id){
+					var result = (people[i].id);
+					children.push(result);
+			}
+			else{
+				continue;
+			}
+		}
+	}
+	return (children);
+}
+
+function family(person){
+	var family = [];
+	var currentSpouse = spouse(person);
+	var currentSiblings = siblings(person);
+	var currentParentals = parentals(person);
+	var currentChildren = children(person)
+	family.push("Spouse: \n" + getFullName(currentSpouse));
+	family.push("\nSiblings: \n" + getFullName(currentSiblings));
+	family.push("\nParents: \n" + getFullName(currentParentals));
+	family.push("\nChildren: \n" + getFullName(currentChildren));
+	return(family.join("\n"));
+}
+
+function kin(person){
+	var nextOfKin;
+	
+	var currentSpouse = person.currentSpouse;
+	var currentChildren = children(person);
+	var currentParents = parentals(person);
+	var currentSiblings = siblings(person);
+	
+	if (currentSpouse == null){
+		if (currentChildren.length == 0){
+			if (currentParents.length == 0){
+				if(currentSiblings.length == 0){
+					alert(person.firstName + " " + person.lastName + " has no next of kin.");
+				}
+				else if (currentSiblings.length == 1){
+					nextOfKin = getFullName(currentSiblings);
+				}
+				else if (currentSiblings > 1){
+					for (var i=0; i < currentSiblings.length; i++){
+						var siblingArray = [];
+						siblingArray.push(getPersonInfoById(currentSiblings[i]));
+						for (var j=0; j < siblingArray.length; j++){
+							var eldest = findEldest(getAge(siblingArray[j]));
+							nextOfKin = getFullName(eldest.id);
+						}
+					}		
+				}
+			}
+			else if (currentParents.length == 1){
+				nextOfKin = getFullName(currentParents);
+			}
+			else if (currentParents.length > 1){
+				for (var i=0; i < currentParents.length; i++){
+					var [parentOne,parentTwo] = currentParents;
+					getPersonInfoById(parentOne);
+					getPersonInfoById(parentTwo);
+					if (getAge(parentOne.dob) > getAge(parentTwo.dob)){
+						nextOfKin = getFullName(parentOne.id);
+					}
+					else{
+						nextOfKin = getFullName(parentTwo.id);
+					}
+				}
+			}
+		}
+		else if (currentChildren.length == 1){
+			nextOfKin = getFullName(currentChildren);
+		}
+		else if (currentChildren.length > 1){
+			for (var i=0; i < currentChildren.length; i++){
+				var childArray = [];
+				childArray.push(getPersonInfoById(currentChildren[i]));
+				for (var j=0; j < childArray.length; j++){
+					var eldest = findEldest(getAge(childArray[j]));
+					nextOfKin = getFullName(eldest.id);
+				}
+			}
+		}
+	}
+	else if (currentSpouse != null){
+	nextOfKin = getFullName(spouse(person));}
+	return nextOfKin;
+}
+function spouse(person){
+	var spouse =[];
+	for (var i=0; i < people.length; i++){
+		if (people[i].currentSpouse != null){
+			if (people[i].currentSpouse == person.id){
+				var result = (people[i].id);
+				spouse.push(result);
+			}
+			else{
+				continue;
+			}
+		}
+	}
+	return (spouse);
+}
+
+function siblings(person){
+	var siblings =[];
+	for (var i=0; i < people.length; i++){
+		if (people[i].parents.length !=0){
+			if((people[i].parents[0] == person.parents[0] || people[i].parents[0] == person.parents[1] || people[i].parents[0] == person.parents[1] || people[i].parents[1] == person.parents[1]) && people[i].id != person.id){
+				var results = (people[i].id);
+				siblings.push(results);
+			}
+			else{
+				continue;
+			}
+		}
+	}
+	return (siblings);
+}
+
+function parentals(person){
+	var parentals = [];
+	for (var i=0; i < people.length;i++){
+		if (people[i].id == person.parents[0] || people[i].id == person.parents[1]){
+			var results = (people[i].id);
+			parentals.push(results);
+		}
+		else{
+			continue;
+		}
+	}
+	return (parentals);
+}
+
+function getTraitInfo(traits){
+	var peopleTraitsArray = [];
+	
+	var ageRangeMin = '';
+	var ageRangeMax = '';
+	
+	/*for (var k = 0; k < traits.length; k++) {
+		if(traits[k].indexOf('-') >= 0){
+			var [ageRangeMin,ageRangeMax] = traits[k].split('-');
+		}
+	}*/
+	
+	for (var i=0; i < people.length; i++){
+		for (var j=0; j < traits.length; j++){
+			if (traits[j] == getAge(people[i].dob)){peopleTraitsArray.push(portfolio(people[i]));}
+			else if (traits[j].ageRangeMin > getAge(people[i].dob) && traits[j].ageRangeMax < getAge(people[i].dob)){peopleTraitsArray.push(portfolio(people[i]));}
+			else if (traits[j] == getHeight(people[i].height)){peopleTraitsArray.push(portfolio(people[i]));}
+			else if (traits[j] == getWeight(people[i].weight)){peopleTraitsArray.push(portfolio(people[i]));}
+			else if (traits[j] == people[i].occupation){peopleTraitsArray.push(portfolio(people[i]));}
+			else if (traits[j] == people[i].eyeColor){peopleTraitsArray.push(portfolio(people[i]));}
+		}
+	}
+	return (peopleTraitsArray.join("\n"));
+}
+
+/*function getTraitInfoTwo(traits){
+	var traitsResults = [];
+
+	var traitsAge = new RegExp('/^\d\d$/');
+	var traitsAgeRange = new RegExp('/^\d\d\W\d\d$/');
+	var traitsHeight = new RegExp('/^\d\W\d\W\W/');
+	var traitsWeight = new RegExp('/[0-9]{2,3}[lbs]/');
+	var traitsOccupation = new RegExp('/^\D/');
+	var traitsEyeColor = new RegExp('/^\D/');
+	
+	for(var i=0; i < traits.length; i++){
+		if (var traits[i] == traitsAge){
+			traitsResults.push(traitsAge);
+		}
+		else if (var traits[i] == traitsAgeRange){
+			traitsResults.push(traitsAgeRange);
+		}
+		else if (var traits[i] == traitsHeight){
+			traitsResults.push(traitsHeight);
+		}
+		else if (var traits[i] == traitsWeight){
+			traitsResults.push(traitsWeight);
+		}
+		else if (var traits[i] == traitsOccupation){
+			traitsResults.push(traitsOccupation);
+		}
+		else if (var traits[i] == traitsEyeColor){
+			traitsResults.push(traitsEyeColor);
+		}
+	}
+	return traitsResults;
+}*/
+
+function peopleMapSearch(traits){
+	var peopleAge = people.map(function(x){if (getAge(x.dob) == traits){return (x);}});
+	var peopleAgeRage = people.map(function(x){if (getAge(x.dob) < traits || getAge(x.dob) > traits){return (x);}});
+	var peopleHeight = people.map(function(x){if (getHeight(x.height) == traits){return (x);}});
+	var peopleWeight = people.map(function(x){if (getWeight(x.weight) == traits){return (x);}});
+	var peopleOccupation = people.map(function(x){if (x.occupation == traits){return (x);}});
+	var peopleEyeColor = people.map(function(x){if (x.eyeColor  == traits){return (x);}});
+
+	return peopleTraitsResults;
+}
+
+function getAge(dob){
+	var today = new Date;
+	var birthDate = new Date(dob);
+	var age = (today.getUTCFullYear() - birthDate.getUTCFullYear());
+	return age;
+}
+
+function findEldest(peopleArray){
+	var parentOne = peopleArray[0];
+	var parentOne = peopleArray[1];
+	if (parentOne > parentTwo){
+		return parentOne;
+	}
+	else{
+		return parentTwo;
+	}
+}
+
+function getHeight(height){
+	var feet = Math.floor(height / 12);
+	var inches = (height % 12);
+	var result = (feet + "'" + inches + "''");
+	return result;
+}
+
+function getWeight(weight){
+	return weight+"lbs";
+}
+
+function Person(id, firstName, lastName, gender, dob, height, weight, eyeColor, occupation, parents, currentSpouse){
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.gender = gender;
+	this.dob = dob;
+	this.height = height;
+	this.weight = weight;
+	this.eyeColor = eyeColor;
+	this.occupation = occupation;
+	this.parents = parents;
+	this.currentSpouse = currentSpouse;
+}
+
+function peopleArray(){
+	var index = 0;
+	for(var key in dataObject){
+		people[index] = new Person(key, dataObject[key].firstName, dataObject[key].lastName, dataObject[key].gender, dataObject[key].dob, dataObject[key].height, dataObject[key].weight, dataObject[key].eyeColor, dataObject[key].occupation, dataObject[key].parents, dataObject[key].currentSpouse);
+		index++;
+	}
+}
